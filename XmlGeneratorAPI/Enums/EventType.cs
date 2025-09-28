54837b80f4b4ee60ba0d979ba11cdb28f952938c
Khaled Hussein <khaled.hussein@gs1eg.org>
2025-09-28 16:26:54 +0300

    enums

---
+++ XmlGeneratorAPI/Enums/EventType.cs
@@ -0,0 +1,45 @@
+﻿namespace XmlGeneratorAPI.Enums
+{
+    public enum BizStep
+    {
+        Commissioning =1,
+        Packing,
+        Shipping,
+        VoidShipping,
+        Receiving,
+        ReceivingReturning,
+        PartialReceivingReturning,
+        Unpacking,
+        Sampling,
+        Missing,
+        Destroy,
+        ErrorDeclaration
+    }
+
+    public enum EventAction
+    {
+        Add = 1,
+        Delete,
+        Observe 
+    }
+    public enum EventType
+    {
+        Object = 1,
+        Aggregation,
+        Transaction,
+        Transformation
+    }
+
+    public enum Disposition
+    {
+        Active=1,
+        InProgress,       
+        InTransit,
+        Returned,
+        Stolen,
+        Destroyed,
+        RecallPending
+    }
+
+
+}
