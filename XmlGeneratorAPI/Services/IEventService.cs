0092784d8b65f95de0bc095b2594bbb16c887b66
Hanan Nady <hanan.nady@gs1eg.org>
2025-09-29 13:48:36 +0300

    Revert "solve conflicts"
    This reverts commit c2a1022ba1018055d04ace4231a8efb16984a13e.

--- XmlGeneratorAPI/Services/IEventService.cs
+++
@@ -1,9 +0,0 @@
-﻿using XmlGeneratorAPI.Requests;
-
-namespace XmlGeneratorAPI.Services
-{
-    public interface IEventService
-    {
-        Task<string> CreateEvent(EpcisEventRequest request, IFormFile csvFile);
-    }
-}
