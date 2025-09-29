8df89be135add9bc3a73f909f637d147a7e39a44
Hanan Nady <hanan.nady@gs1eg.org>
2025-09-29 14:02:40 +0300

    add request and validation add resource file

---
+++ XmlGeneratorAPI/Requests/EpcisEventRequest.cs
@@ -0,0 +1,29 @@
+﻿using XmlGeneratorAPI.Enums;
+
+namespace XmlGeneratorAPI.Requests
+{
+    public class EpcisEventRequest
+    {
+        // Core event attributes
+        public DateTime EventTime { get; set; } 
+        public BizStep BizStep { get; set; }
+        public string ReadPoint { get; set; }
+        public string BizLocation { get; set; }
+
+        // Identifiers
+        public string ParentID { get; set; }   // SSCC in aggregation
+
+        // Source / Destination
+        public string SourceType { get; set; }
+        public string DestinationType { get; set; }
+
+        // Product / Lot details
+        public string LotNumber { get; set; }
+        public DateOnly ItemExpirationDate { get; set; }
+
+        // Correction / Exception handling
+        public DateTime DeclarationTime { get; set; } 
+        public string Reason { get; set; }
+        public string CorrectiveEventID { get; set; }
+    }
+}
---
+++ XmlGeneratorAPI/Services/EventService.cs
@@ -0,0 +1,12 @@
+﻿using XmlGeneratorAPI.Requests;
+
+namespace XmlGeneratorAPI.Services
+{
+    public class EventService : IEventService
+    {
+        public async Task<string> CreateEvent(EpcisEventRequest request, IFormFile csvFile)
+        {
+            return "Test";
+        }
+    }
+}
