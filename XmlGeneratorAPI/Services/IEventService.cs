c2a1022ba1018055d04ace4231a8efb16984a13e
Hanan Nady <hanan.nady@gs1eg.org>
2025-09-29 13:33:07 +0300

    solve conflicts

---
+++ XmlGeneratorAPI/Services/IEventService.cs
@@ -0,0 +1,9 @@
+﻿using XmlGeneratorAPI.Requests;
+
+namespace XmlGeneratorAPI.Services
+{
+    public interface IEventService
+    {
+        Task<string> CreateEvent(EpcisEventRequest request, IFormFile csvFile);
+    }
+}
